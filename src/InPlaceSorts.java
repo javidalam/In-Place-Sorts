a5ce8fb9bf2013fc2dc3b93081940f509a2c92ce
javid <jalam6556@bths.edu>
2018-11-21 22:20:38 -0500

    fgs x hgxgffmy  k u g

--- src/InPlaceSorts.java
+++ src/InPlaceSorts.java
@@ -3,7 +3,7 @@ public class InPlaceSorts
     public static void bubbleSort(String[] list1)
     {
         int swap = 1;
-        while( swap != 0)
+        while(swap != 0)
         {
             for(swap = 0; swap < list1.length; swap++)
             {
@@ -19,8 +19,63 @@ public class InPlaceSorts
             }
         }
     }
-    public static void selectionSort(double[] list1)
+    public void insertionSort(int[] list1)
     {
-        int
+        for(int i = 0; i < list1.length; i++)
+        {
+            for(int j = i; j > 0; j--)
+            {
+                if(list1[j - 1] > list1[j])
+                {
+                    int temp = list1[j];
+                    list1[j - 1] = list1[j];
+                    list1[j - 1] = temp;
+                }
+                else
+                {
+                    j = 0;
+                }
+            }
+        }
+    }
+    public void mergeSort(int[] list1, int left, int mid, int right, int[] temp)
+    {
+        int i;
+        int j;
+        int k;
+
+        i = left;
+        j = mid + 1;
+        k = left;
+        while(i <= mid && j <= right)
+        {
+            if (list1[i] < list1[j])
+            {
+              list1[i] = temp[k];
+              i++;
+            }
+            else
+            {
+                list1[j] = temp[k];
+                j++;
+            }
+            k++;
+        }
+        while(i <= mid)
+        {
+            list1[i] = temp[k];
+            i++;
+            k++;
+        }
+        while(j <= right)
+        {
+            list1[j] = temp[k];
+            j++;
+            k++;
+        }
+        for(int l = 0; l < temp.length; l++)
+        {
+
+        }
     }
 }
